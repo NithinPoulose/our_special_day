af5d25010c736253496eaf447f0eb7d941b7807a
nithinpoulosek <nithinpoulosek@users.noreply.github.com>
2026-04-22 15:02:52 +0530

    deployment

--- src/app/services/notification.service.ts
+++ src/app/services/notification.service.ts
@@ -85,11 +85,9 @@ export class NotificationService {
 
   private scheduleOneTime(): void {
     const targets = [
-      { time: '2026-04-22T00:45:00', title: '💕 Nithin & Neeraja', body: '127 days to go... Every second with you is a gift! ✨' },
-      { time: '2026-04-22T14:48:00', title: '💕 Nithin & Neeraja', body: '127 days to go... Can\'t wait to start forever with you! 💍' },
-      { time: '2026-04-22T14:50:00', title: '💕 Nithin & Neeraja', body: '127 days to go... Can\'t wait to start forever with you! 💍' },
-      { time: '2026-04-22T14:55:00', title: '💕 Nithin & Neeraja', body: '127 days to go... Can\'t wait to start forever with you! 💍' },
-      { time: '2026-04-22T23:00:00', title: '💕 Nithin & Neeraja', body: '127 days to go... Goodnight my love, dreaming of our forever! 🌙' },
+      { time: '2026-04-22T15:10:00', title: '💕 Nithin & Neeraja', body: '127 days to go... Every second with you is a gift! ✨' },
+      { time: '2026-04-22T15:12:00', title: '💕 Nithin & Neeraja', body: '127 days to go... Can\'t wait to start forever with you! 💍' },
+      { time: '2026-04-22T15:15:00', title: '💕 Nithin & Neeraja', body: '127 days to go... Goodnight my love, dreaming of our forever! 🌙' },
     ];
     for (const { time, title, body } of targets) {
       const delay = new Date(time).getTime() - Date.now();
